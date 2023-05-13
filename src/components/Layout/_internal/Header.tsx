f020ff66f392775e310f218763eaf549c308491d
Yutan0423 <yuutan3240@gmail.com>
2023-05-13 19:26:55 +0900

    create profile

--- src/components/Layout/_internal/Header.tsx
+++ src/components/Layout/_internal/Header.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <header className='px-6 py-4 text-xl'>tanblog</header>
+    <header className="px-6 py-4 text-xl">
+      <Link href="/">tanblog</Link>
+    </header>
   )
 }
 
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,35 +1,43 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { Spacer } from '../Util/Spacer'
 
 const Profile = () => {
   return (
-    <div className='flex'>
-      <div className='rounded-full'>
-        <Image
-          src={'/icon_me.png'}
-          height={96}
-          width={96}
-          alt='icon_me'
-        />
-      </div>
-      <Spacer size={8} />
+    <div className="flex items-center shadow-md rounded-lg py-6 px-12">
+      <Image
+        src="/icon_me.png"
+        alt="icon_me"
+        className="rounded-full"
+        height={80}
+        width={80}
+      />
+      <Spacer size={4} />
       <div>
-        <div className='flex'>
+        <div className="flex items-center">
           <span>たん</span>
-          <Image
-          src='/icon_github.png'
-          height={24}
-          width={24}
-          alt='icon_github'
-          />
-          <Image
-          src='/icon_twitter.png'
-          height={24}
-          width={24}
-          alt='icon_twitter'
-          />
+          <Spacer size={4} />
+          <Link href="https://github.com/Yutan0423">
+            <Image
+              src="/icon_github.png"
+              height={24}
+              width={24}
+              alt="icon_github"
+            />
+          </Link>
+          <Spacer size={3} />
+          <Link href="https://twitter.com/hametsuno">
+            <Image
+              src="/icon_twitter.png"
+              height={24}
+              width={24}
+              alt="icon_twitter"
+            />
+          </Link>
         </div>
+        <Spacer size={3} />
+        <p>Web開発が好き</p>
         <p>Typescript / Next.js / golangなど</p>
       </div>
     </div>
